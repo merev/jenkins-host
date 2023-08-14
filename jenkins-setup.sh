a0c218966dcd60176e06f4c388cb08a2276dbdba
merev <101055426+merev@users.noreply.github.com>
2023-08-14 12:00:45 +0300

    Update jenkins-setup.sh

--- jenkins-setup.sh
+++ jenkins-setup.sh
@@ -2,13 +2,16 @@
 
 echo "* Install Java version 11 ..."
 apt-get update
-apt-get install -y fontconfig openjdk-11-jre curl git
+apt-get install -y fontconfig openjdk-17-jre curl git
 
 echo "* Download the key ..."
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 echo "* Create record for the repository ..."
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 echo "* Update package information and install Jenkins ..."
 apt-get update
